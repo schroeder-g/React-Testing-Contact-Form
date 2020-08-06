3cd5b0d4fd12d7a06b468b02118fe9cea5060958
Alex <schroedergoncalves@gmail.com>
2020-08-06 18:45:51 -0400

    innit commit 30% done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,24 +14,24 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor="firstName">First Name*
           <input
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
-          />
+          /></label>
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor="lastName">Last Name*
           <input
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
-          />
+          /></label>
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
           )}
@@ -40,22 +40,23 @@ const ContactForm = () => {
         <div>
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
+           <input name="email" ref={register({ required: true })} />
           </label>
-          <input name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <label htmlFor="message">Message
+            <textarea name="message" ref={register({ required: false })} />
+          </label>
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submit" type="submit" />
       </form>
     </div>
   );
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,38 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react'
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import { render, fireEvent, getAllByDisplayValue, queryAllByLabelText, getByText, screen } from '@testing-library/react'
+import ContactForm from './components/ContactForm'
+import { act } from 'react-dom/test-utils';
+
+test("Sanity Test", () => {
+    render(<ContactForm />)
+})
+
+test("Ensure that you can fill out the form with valid data and submit a new user", () => {
+//Arrange: grab the contact form component and its inputs
+    render(<ContactForm />)
+    const fName = screen.getByLabelText(/first name/i)
+    const lName = screen.getByLabelText(/last name/i)
+    const email = screen.getByLabelText(/email/i)
+    const message = screen.getByLabelText(/message/i)
+    const submit = screen.getByTestId("submit")
+
+//Act: click on the form elements and enter valid data
+    act(
+        fireEvent.click(fName),
+        fireEvent.input(fName, {target: "Monsieur"}),
+        fireEvent.input(lName, { target: "Mikado"}),
+        fireEvent.input(email, { target: "donMikado@gmail.com"}),
+        fireEvent.input(message, { target: "Greetings good sir, thank you for your service"}),
+        fireEvent.click(submit)
+    )
+
+
+    //Assert: 
+    const formErrs = screen.getAllByText(/Error/i)
+    expect(formErrs).not.toBeInTheDocument()
+})
